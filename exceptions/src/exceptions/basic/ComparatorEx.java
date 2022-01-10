460c0e0182277f9c40e00b6aa4b7fff43d3a00e1
sarojakilesh <akileshsaroj@gmail.com>
2022-01-10 14:46:17 +0530

    zcsc

--- exceptions/src/exceptions/basic/ComparatorEx.java
+++ exceptions/src/exceptions/basic/ComparatorEx.java
@@ -11,7 +11,7 @@ import java.util.List;
 		Student3 student3 = new Student3("akilesh", "CSE", 88000.00);
 		Student3 student4 = new Student3("ganesh", "CIVIL", 67000.00);
 		Student3 student5 = new Student3("rakesh", "CSE", 78000.00);
-		Student3 student6 = new Student3("surendra", "IT", 88000.00);
+		Student3 student6 = new Student3("surendraAA", "IT", 88000.00);
 
 		List<Student3> list = Arrays.asList(student1, student2, student3, student4, student5, student6);
 		StudentsIDCompare sid = new StudentsIDCompare();
